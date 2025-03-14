f8a9aca141b129b823cdcfe173653efcc5562672
Vanillin <ivan.elistrat@mail.ru>
2025-03-14 16:16:16 +0300

    Fix problem with version projects

--- scr/Application/DependencyInjection.cs
+++ scr/Application/DependencyInjection.cs
@@ -1,11 +1,6 @@
 ﻿using Application.Mappings;
 using Application.Services;
 using Microsoft.Extensions.DependencyInjection;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Application
 {
--- scr/Application/Mappings/MappingProfile.cs
+++ scr/Application/Mappings/MappingProfile.cs
@@ -1,12 +1,6 @@
 ﻿using Application.Dto;
 using AutoMapper;
 using Domain.Entities;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net.Mail;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Application.Mappings
 {
@@ -14,7 +8,7 @@ namespace Application.Mappings
     {
         public MappingProfile()
         {
-            CreateMap<Domain.Entities.Attachment, AttachmentDto>().ReverseMap();
+            CreateMap<Attachment, AttachmentDto>().ReverseMap();
             CreateMap<Product, ProductDto>().ReverseMap();
             CreateMap<Review, ReviewDto>().ReverseMap();
             CreateMap<Section, SectionDto>().ReverseMap();
